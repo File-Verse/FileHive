88e572b9438b5f8ae784c2f018b3588dd02a362a
priyanshu chouhan <priyanshuchouhan@9752@gmail.com>
2026-04-19 15:29:17 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,132 @@
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  ServerSocket? server;
+  Socket? socket;
+  List<String> messages = [];
+  TextEditingController controller = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // 🔹 START SERVER
+  void startServer() async {
+    try {
+      server = await ServerSocket.bind(InternetAddress.anyIPv4, 3000);
+
+      setState(() {
+        messages.add("Server started");
+      });
+
+      server!.listen((client) {
+        socket = client;
+
+        client.listen((data) {
+          String msg = utf8.decode(data);
+          setState(() {
+            messages.add("Client: $msg");
+          });
+        });
+      });
+    } catch (e) {
+      setState(() {
+        messages.add("Server Error");
+      });
+    }
+  }
+
+  // 🔹 CONNECT TO SERVER
+  void connectToServer() async {
+    try {
+      socket = await Socket.connect("10.66.212.46", 3000)
+          .timeout(Duration(seconds: 5));
+
+      setState(() {
+        messages.add("Connected to server");
+      });
+
+      socket!.listen((data) {
+        String msg = utf8.decode(data);
+        setState(() {
+          messages.add("Server: $msg");
+        });
+      });
+    } catch (e) {
+      setState(() {
+        messages.add("Connection Failed");
+      });
+    }
+  }
+
+  // 🔹 SEND MESSAGE
+  void sendMessage() {
+    if (socket != null && controller.text.isNotEmpty) {
+      socket!.write(controller.text);
+
+      setState(() {
+        messages.add("Me: ${controller.text}");
+      });
+
+      controller.clear();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: Text("FileHive Chat")),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: startServer,
+            child: Text("Start Server"),
+          ),
+
+          ElevatedButton(
+            onPressed: connectToServer,
+            child: Text("Connect"),
+          ),
+
+          Expanded(
+            child: ListView(
+              children: messages
+                  .map((msg) => ListTile(title: Text(msg)))
+                  .toList(),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: controller,
+                  decoration: InputDecoration(hintText: "Enter message"),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.send),
+                onPressed: sendMessage,
+              )
+            ],
+          )
+        ],
       ),
     );
   }
