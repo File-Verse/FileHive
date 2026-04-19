094d31db250e7c65d322d2c509bf93dbbb544594
Bamnhekarmayank <bamnhekarmayank@gmail.com>
2026-04-19 11:52:10 +0530

    feat: local_server_service and upload_handler complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Server test
+
+
   runApp(const MyApp());
 }
 
@@ -104,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('hello manku:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
